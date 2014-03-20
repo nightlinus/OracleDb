a8cce4244cb44a7ed5aec1fa694bc520dd5f1101
nightlinus <m.a.ogarkov@gmail.com>
2014-03-20 13:35:00 +0400

    feat implement psr-4 autoloading

--- src/Db.php
+++ src/Db.php
@@ -112,42 +112,6 @@ class Db
     }
 
     /**
-     * OracleDb PSR-0 autoloader
-     */
-    public static function autoload($className)
-    {
-        $thisClass = str_replace(__NAMESPACE__ . '\\', '', __CLASS__);
-
-        $baseDir = __DIR__;
-
-        if (substr($baseDir, -strlen($thisClass)) === $thisClass) {
-            $baseDir = substr($baseDir, 0, -strlen($thisClass));
-        }
-
-        $className = ltrim($className, '\\');
-        $fileName = $baseDir;
-        if (($lastNsPos = strripos($className, '\\')) !== false) {
-            $namespace = substr($className, 0, $lastNsPos);
-            $className = substr($className, $lastNsPos + 1);
-            $fileName .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-        }
-        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-        if (file_exists($fileName)) {
-            /** @noinspection PhpIncludeInspection */
-            require $fileName;
-        }
-    }
-
-    /**
-     * Register PSR-0 autoloader
-     */
-    public static function registerAutoloader()
-    {
-        spl_autoload_register(__NAMESPACE__ . "\\Db::autoload");
-    }
-
-    /**
      *  Освобождаем ресурсы в деструкторе
      */
     public function __destruct()
