3c5a59e40494913b80998697e5ac6fab9c917f38
nightlinus <m.a.ogarkov@gmail.com>
2014-04-11 19:44:38 +0400

    feat (StatementCache.php) new class to handle statement cahe

---
+++ src/StatementCache.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Date: 03.04.14
+ * Time: 14:53
+ *
+ * @category 
+ * @package  OracleDb
+ * @author   nightlinus <user@localhost>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @version  
+ * @link     
+ */
+
+namespace OracleDb;
+
+
+class StatementCache {
+
+    /**
+     * @var Statement[];
+     */
+    protected $hashCache = [];
+
+    /**
+     * @var Statement[]
+     */
+    protected $orderCache = [];
+
+    protected $cacheSize;
+
+    /**
+     * @return mixed
+     */
+    public function getCacheSize()
+    {
+        return $this->cacheSize;
+    }
+
+    /**
+     * @param mixed $cacheSize
+     */
+    public function setCacheSize($cacheSize)
+    {
+        $this->cacheSize = $cacheSize;
+        $this->cleanOldCache();
+    }
+
+    /**
+     * @param $cacheSize
+     */
+    public function __construct($cacheSize)
+    {
+        $this->cacheSize = $cacheSize;
+    }
+
+    /**
+     * @param $statement Statement
+     */
+    public function add($statement)
+    {
+        $hash = $this->getHash($statement);
+        $this->hashCache[ $hash ] = $statement;
+        array_push($this->orderCache, $statement);
+        $this->cleanOldCache();
+    }
+
+    /**
+     * @param $sql string
+     *
+     * @return null
+     */
+    public function get($sql)
+    {
+        $hash = $this->getHash($sql);
+
+        return isset($this->hashCache[ $hash ]) ? $this->hashCache[ $hash ] : null;
+    }
+
+    /**
+     * @return $this
+     */
+    protected function cleanOldCache()
+    {
+        while (count($this->orderCache) >= $this->cacheSize) {
+            $statement = array_shift($this->orderCache);
+            $hash = $this->getHash($statement);
+            unset($this->hashCache[ $hash ]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param $statement Statement|string
+     *
+     * @return string
+     */
+    protected function getHash($statement)
+    {
+        $sql = $statement;
+        if ($statement instanceof Statement) {
+            $sql = $statement->getQueryString();
+        }
+
+        return hash('md5', $sql);
+    }
+} 
