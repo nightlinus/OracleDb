bafd0d2ca41204e01acba25166cf487a03c8baa5
nightlinus <m.a.ogarkov@gmail.com>
2014-04-21 17:56:53 +0400

    docs (StatementCache.php) php-docs fixes

--- src/StatementCache.php
+++ src/StatementCache.php
@@ -149,11 +149,9 @@ class StatementCache implements \IteratorAggregate {
     }
 
     /**
-     * (PHP 5 &gt;= 5.0.0)<br/>
      * Retrieve an external iterator
      * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
-     * @return \Traversable An instance of an object implementing <b>Iterator</b> or
-     * <b>Traversable</b>
+     * @return \Iterator
      */
     public function getIterator()
     {
