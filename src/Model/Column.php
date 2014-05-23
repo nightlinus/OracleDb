871b7fbd71c9d58027fcba6142be35c995e94494
nightlinus <m.a.ogarkov@gmail.com>
2014-05-23 22:44:00 +0400

    feat (Column.php) add column container

---
+++ src/Model/Column.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Date: 23.05.14
+ * Time: 16:52
+ *
+ * @category
+ * @package  OracleDb
+ * @author   nightlinus <user@localhost>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @version
+ * @link
+ */
+
+namespace nightlinus\OracleDb\Model;
+
+/**
+ * Class Column
+ * @package nightlinus\OracleDb\Model
+ */
+class Column
+{
+
+    protected $id;
+
+    protected $length;
+
+    protected $name;
+
+    protected $nullable;
+
+    protected $precision;
+
+    protected $scale;
+
+    protected $type;
+
+    /**
+     * @param $id
+     * @param $length
+     * @param $name
+     * @param $nullable
+     * @param $precision
+     * @param $scale
+     * @param $type
+     */
+    public function __construct($id, $length, $name, $nullable, $precision, $scale, $type)
+    {
+        $this->id = $id;
+        $this->length = $length;
+        $this->name = $name;
+        $this->nullable = $nullable;
+        $this->precision = $precision;
+        $this->scale = $scale;
+        $this->type = $type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLength()
+    {
+        return $this->length;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNullable()
+    {
+        return $this->nullable;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrecision()
+    {
+        return $this->precision;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getScale()
+    {
+        return $this->scale;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
