0f52ff82010df42f742f7bb01876201195b0ec40
nightlinus <m.a.ogarkov@gmail.com>
2014-09-11 13:27:00 +0400

    fix (autoload.php) package prefix

--- autoload.php
+++ autoload.php
@@ -13,7 +13,7 @@
 spl_autoload_register(function ($class) {
 
     // project-specific namespace prefix
-    $prefix = 'OracleDb\\';
+    $prefix = 'nightlinus\\OracleDb\\';
 
     // base directory for the namespace prefix
     $base_dir = __DIR__ . '/src/';
